f22d289f32a0d40cf091253c89cef1970c6d255a
Camila Javiera <camijavinavarro@gmail.com>
2026-05-10 15:24:21 -0600

    implement array input and flow control logic in Program.cs

--- FlowControlArrays/Program.cs
+++ FlowControlArrays/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] numbers = new int[8];
+        Console.Clear();
+        Console.WriteLine("=== Flow Control Practice with Arrays ===");
+        Console.WriteLine("");
+        Console.WriteLine("Please enter 8 numbers ");
+
+        for (int i = 0; i < 8; i++){ 
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine();
+ 
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < 0)
+            {
+                continue;
+            }
+
+            if (numbers[i] == 0)
+            {
+                break;
+            }
+
+            Console.Write($"{numbers[i]}, ");
+        }
+
     }
 }
